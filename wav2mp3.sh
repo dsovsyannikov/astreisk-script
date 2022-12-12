3808d8641cd599697ebf206d12f95fa0a81269f6
dsovsyannikov <91129436+dsovsyannikov@users.noreply.github.com>
2022-12-12 17:36:43 +0500

    Update wav2mp3.sh

--- wav2mp3.sh
+++ wav2mp3.sh
@@ -9,28 +9,33 @@ find /var/spool/asterisk/monitor/ -type f -mtime +700 -exec rm -rf {} \;
 
 # delete small empty wav files
 find /var/spool/asterisk/monitor -type f -name *.wav -size -45c -delete
-
+uid=0
 
 while [ 1 ]
 do
-sleep 1s
-result="$(mysql -B -N -e "SELECT recordingfile,uniqueid,date(calldate) FROM asteriskcdrdb.cdr WHERE calldate < CURDATE() AND recordingfile LIKE '%wav' ORDER BY calldate DESC LIMIT 1")"
+# sleep 1s
+result="$(mysql -B -N -e "SELECT recordingfile,uniqueid,date(calldate) FROM asteriskcdrdb.cdr WHERE uniqueid >='$uid' AND calldate < CURDATE() AND recordingfile LIKE '%wav' ORDER BY uniqueid LIMIT 1")"
 echo $result
-wavfilenopath="$(echo $result | awk '{ print $1;}')"
-id="$(echo $result | awk '{ print $2;}' | sed -r 's/\..+//')"
-dirdate="$(echo $result | awk '{ print $3;}' | sed 's/-/\//g')"
-dt="$(echo $result | awk '{ print $3;}')"
-echo $dirdate
-echo $wavfilenopath
+wavfilenopath="$(echo "$result" | awk -F '\t' '{ print $1;}')"
+id="$(echo "$result" | awk -F '\t' '{ print $2;}' | sed -r 's/\..+//')"
+uid="$(echo "$result" | awk -F '\t' '{ print $2;}')"
+dirdate="$(echo "$result" | awk -F '\t' '{ print $3;}' | sed 's/-/\//g')"
+dt="$(echo "$result" | awk -F '\t' '{ print $3;}')"
+
 if [ -z "$wavfilenopath" ]
 then
-  echo "\$wavfilenopath empty."
+  echo "\$wavfilenopath not define. Exit"
   exit 1
 else
-  echo "\$wavfilenopath NOT empty."
-wavfile="$(find /var/spool/asterisk/monitor/$dirdate/ -name $wavfilenopath -print)"
+  echo "\$wavfilenopath define."
+wavfile="$(echo /var/spool/asterisk/monitor/$dirdate/$wavfilenopath)"
 
 test -e "$wavfile" && {
+actualsize=$(stat -c %s $wavfile)
+if [ $actualsize -lt 45 ]; then
+    rm -f $wavfile
+else
+
 mp3file="$(echo $wavfile | sed 's/wav/mp3/')"
 mp3filenopath="$(echo $wavfilenopath | sed 's/wav/mp3/')"
 echo $wavfile
@@ -41,7 +46,10 @@ stat -c%s $mp3file
 # update CDR record after convert from wav to mp3
 mysql -D asteriskcdrdb <<<"UPDATE cdr SET recordingfile='$mp3filenopath' WHERE uniqueid LIKE '$id%' AND recordingfile='$wavfilenopath'"
 rm -f $wavfile
+fi
+
 } || {
+
 mp3filenopath="$(echo $wavfilenopath | sed 's/wav/mp3/')"
 echo $mp3filenopath
 # update CDR record without convert from wav to mp3
@@ -56,3 +64,5 @@ test -e "$wavfile" && {
 fi
 
 done
+
+#mysql -B -N -e "SELECT recordingfile,uniqueid FROM asteriskcdrdb.cdr WHERE calldate < CURDATE() AND recordingfile LIKE '%wav' ORDER BY calldate DESC LIMIT 1" | awk '{ wavfilenopath = $0; id = $1 }'
